323230b1388c83f20860ce6858dc42c59fe0ec43
jp-the-dev <61920708+jp-the-dev@users.noreply.github.com>
2020-03-07 23:12:39 +0100

    Update rot13-encrypt.py

--- rot13-encrypt.py
+++ rot13-encrypt.py
@@ -1,4 +1,5 @@
 def rot13_encrypt(string):
     text = string
-    rot13_text = "".join([abc[(abc.find(c)+13)%26] for i in text])
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    rot13_text = "".join([alphabet[(alphabet.find(c)+13)%26] for i in text])
     print(rot13_text)
