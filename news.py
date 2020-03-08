6c98c417ac12c032a1bbc0adcc845f84b045d20b
jp-the-dev <61920708+jp-the-dev@users.noreply.github.com>
2020-03-08 01:27:32 +0100

    Update news.py

--- news.py
+++ news.py
@@ -1,22 +1,56 @@
 import time
 import sys
+import urllib.request
+import os
 
 
 def news():
   print("\n")
   print("News")
   print("\n")
-  print("UPDATE: Es wird bald eine 1.2 geben!")
-  print("\n")
-  print("Was wird neu sein?")
-  print("\n")
-  print("- Neue Apps für die App ExtraApps")
-  print("- Whatsapp via Selenium wird verfügbar sein")
-  print("- SecureBrowser (Apache) wird verfügbar sein")
-  print("- JannOSAntivirus wird verfügbar sein")
-  print("- RawWeb (Apache) wird verfügbar sein")
+  print("Hier ist nichts los...")
   print("\n")
   print("Stand: 08.03.2020")
-  print("Zum aktualisieren bitte neuinstallieren oder ab der 1.2")
-  print("in der ExtraApp news aktualisieren auswählen")
+  print("\n")
+  print("[1] News aktualisieren")
+  print("[ENTER] News beenden")
+  eingabe = input("> ")
+  if eingabe == "1":
+    print("\n")
+  else:
+    print()
+    
+  print("\n")
   time.sleep(3)
+  
+ 
+def extra_apps(self):
+    def check_ping():
+        hostname = "www.github.com"
+        response = os.system("ping " + hostname)
+        if response == 0:
+            print("\n")
+            print("Verbindung aufgebaut!")
+            print("\n")
+            return 0
+        else:
+            print("\n")
+            print("Verbindung konnte nicht hergestellt werden!")
+            print("\n")
+            return 1
+     def extra_apps_download(self):
+        url = "https://raw.githubusercontent.com/jp-the-dev/pythonproject/master" + self + ".py"
+        name = self + ".py"
+        urllib.request.urlretrieve(url, name)
+        url = "https://raw.githubusercontent.com/jp-the-dev/pythonproject/master/" + self + "-starter.py"
+        name = self + "-starter.py"
+        urllib.request.urlretrieve(url, name)
+        print("\n")
+        print("Bitte starte JannOS neu!")
+        print("\n")
+        sys.exit()
+     if int(check_ping()) == 0:
+        extra_apps_download(self)
+     elif int(check_ping()) == 1:
+        print("\n")
+extra_apps("news")
