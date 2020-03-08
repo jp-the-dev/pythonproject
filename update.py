ec128a69277ee72c0f78ab18d5e9d5b8550574ff
jp-the-dev <61920708+jp-the-dev@users.noreply.github.com>
2020-03-08 01:05:56 +0100

    Create update.py

---
+++ update.py
@@ -0,0 +1,13 @@
+import time
+import sys
+
+
+def update():
+  print("\n")
+  print("Update")
+  print("\")
+  print("Die neueste Version ist die 1.1 - Max")
+  print("Alsbald wird die Version 1.2 - Tim entwickelt")
+  print("\n")
+  print("Stand: 08.03.2020")
+  time.sleep(3)
